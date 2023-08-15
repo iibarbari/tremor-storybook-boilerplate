f4879ce532050ede93ae0d47568d9e7d5671c3df
Ilknur Ultanir Sari <45620986+iibarbari@users.noreply.github.com>
2023-08-15 10:27:24 +0300

    add bar chart

---
+++ src/components/BarChart/BarChart.stories.js
@@ -0,0 +1,14 @@
+import {default as BarChart} from './index.jsx';
+
+export default {
+    title: 'Components/BarChart',
+    component: BarChart,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'fullscreen',
+    },
+};
+
+export const Default = {
+    args: {},
+};
